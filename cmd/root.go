17a2e49064cb5cf1041b2b08f8781473a44cb7c5
Roald Nefs <info@roaldnefs.com>
2019-03-24 21:06:03 +0100

    Initial commit
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

---
+++ cmd/root.go
@@ -0,0 +1,165 @@
+// Copyright © 2019 Roald Nefs <info@roaldnefs.com>
+
+package cmd
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/prometheus/client_golang/api"
+	"github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	defaultBaseURL = "http://localhost:9090/"
+)
+
+var (
+	// Used for command flags.
+	cfgFile                      string
+	commandSilent, commandFiring string
+
+	// rootCmd represents the base command when called without any subcommands
+	rootCmd = &cobra.Command{
+		Use:   "deukalion",
+		Short: "A brief description of your application",
+		Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			httpAPI := newAPI()
+			handleAlerts(httpAPI)
+		},
+	}
+)
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.deukalion.yaml)")
+
+	// Base URL for API requests to Prometheus. Defaults to localhost, but can
+	// be set to a external hosted Prometheus server. The base URL should
+	// always be specified with a trailing slash.
+	rootCmd.PersistentFlags().StringP("url", "u", defaultBaseURL, "Promtheus URL")
+	viper.BindPFlag("url", rootCmd.PersistentFlags().Lookup("url"))
+
+	//
+	rootCmd.Flags().StringVarP(&commandSilent, "silent", "s", "", "Command to execute when aren't firing")
+	rootCmd.MarkFlagRequired("silent")
+
+	//
+	rootCmd.Flags().StringVarP(&commandFiring, "firing", "f", "", "Command to execute when alerts are firing")
+	rootCmd.MarkFlagRequired("firing")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".deukalion" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".deukalion")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+// newAPI returns a Promtheus v1 API client.
+func newAPI() v1.API {
+	baseURL := viper.GetString("url")
+
+	client, err := api.NewClient(api.Config{Address: baseURL})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	httpAPI := v1.NewAPI(client)
+
+	return httpAPI
+}
+
+// handleAlerts
+func handleAlerts(httpAPI v1.API) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	alerts, err := httpAPI.Alerts(ctx)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	command := commandSilent
+	if len(alerts.Alerts) > 0 {
+		command = commandFiring
+	}
+
+	err = execute(command)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return nil
+}
+
+// execute
+func execute(command string) error {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
+	commandSlice := strings.Fields(command)
+	name := commandSlice[0]
+	cmd := exec.Command(name)
+	if len(commandSlice) > 1 {
+		args := commandSlice[1:]
+		cmd = exec.Command(name, args...)
+	}
+
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+
+	fmt.Println(stdout.String())
+	fmt.Println(stderr.String())
+
+	return err
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+// Copyright © 2019 Roald Nefs <info@roaldnefs.com>
+
+
+package main
+
+import "github.com/roaldnefs/deukalion/cmd"
+
+func main() {
+	cmd.Execute()
+}
